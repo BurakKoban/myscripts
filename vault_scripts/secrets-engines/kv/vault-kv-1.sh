5bb316f11bdec774057186b0784592e9152fec7f
Burak <burakkoban@gmail.com>
2024-08-15 10:08:48 -0700

    backup files

--- vault_scripts/secrets-engines/kv/vault-kv-1.sh
+++ vault_scripts/secrets-engines/kv/vault-kv-1.sh
@@ -1,6 +1,6 @@
 echo $VAULT_ADDR
 export VAULT_ADDR="https://vault.ops.bcaa.bc.ca"
-export VAULT_TOKEN="hvs.XXXXXXXXXXXXXXXXXXXXXXXX"
+export VAULT_TOKEN="XXX.XXXXXXXXXXXXXXXXXXXXXXXX"
 vault login
 
 # enabling kv secret engine v1 with a custom path
